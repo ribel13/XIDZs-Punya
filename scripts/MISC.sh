5d090010edcdf2534791acd4825e0ffd364d98a9
Atho Belo <181945610+ribel13@users.noreply.github.com>
2026-01-04 11:22:46 +0900

    Update MISC.sh

--- scripts/MISC.sh
+++ scripts/MISC.sh
@@ -76,13 +76,14 @@ download_custom_scripts() {
     log "INFO" "Downloading custom scripts"
     
     local scripts=(
-        "https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/install2.sh|files/root"
+        #"https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/install2.sh|files/root"
         #"https://raw.githubusercontent.com/syntax-xidz/contenx/main/shell/quenxx.sh|files/root"
         #"https://raw.githubusercontent.com/syntax-xidz/contenx/main/shell/tty.sh|files/root"
         #"https://raw.githubusercontent.com/syntax-xidz/contenx/main/shell/xdev|files/usr/bin"
         #"https://raw.githubusercontent.com/syntax-xidz/contenx/main/shell/syntax|files/usr/bin"
         #"https://raw.githubusercontent.com/syntax-xidz/contenx/main/shell/xidz|files/usr/bin"
         #"https://raw.githubusercontent.com/syntax-xidz/contenx/main/shell/x-gpioled|files/usr/bin"
+        "https://github.com/ribel13/XIDZs-Punya/blob/main/scripts/install2.sh|files/root"
         "https://github.com/ribel13/XIDZs-Punya/blob/main/scripts/quenxx.sh|files/root"
         "https://github.com/ribel13/XIDZs-Punya/blob/main/scripts/tty.sh|files/root"
         "https://github.com/ribel13/XIDZs-Punya/blob/main/scripts/xdev|files/usr/bin"
