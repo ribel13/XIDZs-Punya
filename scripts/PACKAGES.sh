44287aaa249ad96e34e7d7731d5c2fc7d131a1ee
Atho Belo <181945610+ribel13@users.noreply.github.com>
2026-01-06 23:46:40 +0900

    Update PACKAGES.sh

--- scripts/PACKAGES.sh
+++ scripts/PACKAGES.sh
@@ -49,12 +49,12 @@ packages_custom+=(
     "luci-app-ramfree_|${REPOS[IMMORTALWRT]}/luci"
     "luci-app-modemband_|${REPOS[IMMORTALWRT]}/luci"
     "luci-app-sms-tool-js_|${REPOS[IMMORTALWRT]}/luci"
-    "dns2tcp_|${REPOS[IMMORTALWRT]}/packages"
+    #"dns2tcp_|${REPOS[IMMORTALWRT]}/packages"
     #"luci-theme-argon_|${REPOS[IMMORTALWRT]}/luci"
     #"luci-app-irqbalance_|${REPOS[IMMORTALWRT]}/luci"
     
     "ookla-speedtest_|${REPOS[KIDDIN9]}"
-    "luci-app-eqosplus_|${REPOS[KIDDIN9]}"
+    #"luci-app-eqosplus_|${REPOS[KIDDIN9]}"
     "luci-app-internet-detector_|${REPOS[KIDDIN9]}"
     "internet-detector_|${REPOS[KIDDIN9]}"
     "internet-detector-mod-modem-restart_|${REPOS[KIDDIN9]}"
@@ -68,7 +68,7 @@ packages_custom+=(
     "luci-app-ipinfo_|https://api.github.com/repos/bobbyunknown/luci-app-ipinfo/releases/latest"
     "luci-app-netmonitor_|https://api.github.com/repos/de-quenx/luci-app-netmonitor/releases/latest"
     "luci-theme-argon_|https://api.github.com/repos/de-quenx/luci-theme-argon/releases/latest"
-    "luci-theme-rtawrt_|https://api.github.com/repos/de-quenx/luci-theme-rtawrt/releases/latest"
+    #"luci-theme-rtawrt_|https://api.github.com/repos/de-quenx/luci-theme-rtawrt/releases/latest"
 )
 
 if [[ "${TYPE}" == "OPHUB" || "${TYPE}" == "ULO" ]]; then
